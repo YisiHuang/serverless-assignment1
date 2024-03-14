fda50bb3cd61599eca5144c784caae3ede4cf3c1
YisiHuang <20095257@mail.wit.ie>
2024-03-14 19:09:42 +0000

    Add get the reviews by a specific year for a specific movie.

--- lambdas/getMovieReviewsByReviewer.ts
+++ lambdas/getMovieReviewsByReviewer.ts
@@ -1,34 +1,56 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   try {
     console.log("Event: ", event);
-    const movieId = event.pathParameters?.movieId;
-    const reviewerName = event.pathParameters?.reviewerName; 
+    const { movieId = '' } = event.pathParameters ?? {};
+    const reviewerNameOrYear = event.pathParameters?.reviewerName;
 
-    if (!movieId || !reviewerName) {
+    if (!movieId) {
       return {
-        statusCode: 400, 
+        statusCode: 400,
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({ message: "Missing or invalid movie Id or reviewer name" }),
+        body: JSON.stringify({ message: "Missing or invalid movie Id" }),
       };
     }
+ 
+    let queryInput;
 
-    const queryInput: QueryCommandInput = {
-        TableName: process.env.TABLE_NAME, 
-        KeyConditionExpression: "MovieId = :movieId",
-        FilterExpression: "ReviewerName = :reviewerName", 
-        ExpressionAttributeValues: {
-          ":movieId": parseInt(movieId),
-          ":reviewerName": reviewerName, 
-        },
-    };
+    if (reviewerNameOrYear && /^\d{4}$/.test(reviewerNameOrYear)) {
+      queryInput = {
+          TableName: process.env.TABLE_NAME,
+          KeyConditionExpression: "MovieId = :movieId",
+          FilterExpression: "begins_with(ReviewDate, :year)",
+          ExpressionAttributeValues: {
+              ":movieId": parseInt(movieId),
+              ":year": reviewerNameOrYear,
+          },
+      };
+  } else if (reviewerNameOrYear) {
+      queryInput = {
+          TableName: process.env.TABLE_NAME,
+          KeyConditionExpression: "MovieId = :movieId",
+          FilterExpression: "ReviewerName = :reviewerName",
+          ExpressionAttributeValues: {
+              ":movieId": parseInt(movieId),
+              ":reviewerName": reviewerNameOrYear,
+          },
+      };
+  } else {
+      return {
+          statusCode: 400,
+          headers: {
+              "content-type": "application/json",
+          },
+          body: JSON.stringify({ message: "Reviewer name or year is required" }),
+      };
+  }
 
     const queryOutput = await ddbDocClient.send(new QueryCommand(queryInput));
 
